be1da2c2f136d4b998bdcc947314e6a2aabfa4ca
sandesh <qalamsal8@gmail.com>
2020-04-07 13:39:45 -0500

    git hub url comment added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,15 @@ public class Main {
 	// write your code here
         System.out.println("hello world ! from github.com..");
         System.out.print("Sandesh Lamsal");
-	
+        //System.out.println("Java Automation\n");
+
+        /*
+        //4/6/2020 2:20 pm
+        int[] intArray={100,200,300};
+        for (int a:intArray){
+            System.out.println(a);
+        }
+        */
+        //https://github.com/qalamsal/gitTest_HelloWorld_Project.git
     }
 }
